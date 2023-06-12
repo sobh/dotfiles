22ac2c386b1bf13700149936e6b72e7cd2a22685
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2023-06-12 20:20:57 +0300

    Removed the python 'xkcdpass' dependency.

--- bin/genpass.sh
+++ bin/genpass.sh
@@ -2,44 +2,106 @@
 #
 # Description:	Generate random passphrase.
 #
-# TODO:		Remove the `xkcdpass` dependency.
+# TODO:         Add command line argument for customizablity.
 #
 
-#---- Functions ----------------------------------------------------------------
+#---- Constants ----------------------------------------------------------------
 OS=$(uname -s)
+# Defaults
+DEFAULT_WORD_COUNT=4
+DEFAULT_WORD_LEN=4
+DEFAULT_DIGITS_COUNT=2
+
+WORDS_LIST_FILES="
+$XDG_DATA_HOME/dict/words
+/usr/share/dict/words
+/usr/share/dict/american
+/usr/share/dict/american-english
+/usr/share/dict/british
+/usr/share/dict/british-english
+"
+
+#---- Functions ----------------------------------------------------------------
 # Get random special character
 special() {
-	# OpenBSD has jot
+	DEFAULT_SPECIAL='\-!@$%^&*_+=:|~?/.;'
+	SPECIAL=${1:-$DEFAULT_SPECIAL}
+	# OpenBSD does not have `/dev/urandom`
 	if [ $OS = "OpenBSD" ]; then
-		char=$(jot -r -c 1 33 47)
+		jot -r -c 128 33 47 | tr -dc "$SPECIAL" | fold -w1 | head -n1 |
+			tr -d '\n'
 	else	# Linux
-		SPECIAL='\-!@$%^&*_+=:|~?/.;'
-		char=$(cat /dev/urandom | tr -dc "$SPECIAL" | head -c 1)
+		cat /dev/urandom | tr -dc "$SPECIAL" | head -c 1 | tr -d '\n'
 	fi
-	# Must use quotes for glob expansion suppression of '*', and escape '%'
-	# for `printf`.
-	[ "$char" = '%' ] && char='%%'
-	echo "$char"
 }
 # Get 2 random digits
 digits() {
-	# OpenBSD has jot
+	count=${1:-2}
+	# OpenBSD does not have `/dev/urandom`
 	if [ $OS = "OpenBSD" ]; then
-		jot -r 1 10 99
-	else
-		cat /dev/urandom | tr -dc "0-9" | head -c 2
+		jot -s '' -nr "$count" 10 99
+	else    # Linux
+		cat /dev/urandom | tr -dc "0-9" | head -c "$count"
 	fi
 }
 
+word() {
+	words_list=${1}
+	len=${2:-$DEFAULT_WORD_LEN}
+	# Use 'sort -R' instead of 'shuf' for OpenBSD compatibility
+	grep -E "^[a-zAZ]{$len,$len}\$" $words_list | sort -R | head -1
+}
+
+# Note: In case you pass a delimiter to this function use double quotes, to stop
+#       the `shell` from interpreting as a glob.
+#       words $WORD_COUNT $WORD_LEN "$DELIM"
+words() {
+	words_list=${1}                  # Word's List
+	count=${2:-$DEFAULT_WORD_COUNT} # Words' Count
+	len=${3:-$DEFAULT_WORD_LEN}     # Words' Length
+	delimiter=${4-$(special)}       # Delimiter (Could be empty string)
+	out="$delimiter"
+	for i in $(seq $count); do
+		if [ "$upper" ] ; then
+			out="$out$(word $words_list $len| tr '[a-z]' '[A-Z]')$delimiter"
+			unset upper
+		else
+			out="$out$(word $words_list $len| tr '[A-Z]' '[a-z]')$delimiter"
+			upper='69'
+		fi
+	done
+	echo -n "$out"
+}
 #---- Main ---------------------------------------------------------------------
-WC=3
+# Get words' list file
+WORDS_LIST=''
+for file in $WORDS_LIST_FILES ; do
+	if [ -f "$file" ]; then
+		WORDS_LIST="$file"
+		break
+	fi
+done
+if [ -z $WORDS_LIST ]; then
+	echo "Error: Unable to find a word file out of the following list:"
+	for file in $WORDS_LIST_FILES ; do printf "\t- $file\n"; done
+	exit -1
+fi
+
 PAD=$(special)
-DELIMITER=$(special)
-WORDS=$(xkcdpass --numwords 3 --delimiter "$DELIMITER" --case 'alternating')
-PASSPHRASE="$PAD$PAD$(digits)$DELIMITER$WORDS$DELIMITER$(digits)$PAD$PAD$LF"
+# Ensure the delimiter is different from the padding
+while true; do
+	DELIM=$(special)
+	[ "$DELIM" != "$PAD" ] && break
+done
+
+PASSPHRASE=''
+PASSPHRASE=$PASSPHRASE$PAD$PAD$(digits $DEFAULT_DIGITS_COUNT)
+PASSPHRASE=$PASSPHRASE$(words $WORDS_LIST $DEFAULT_WORD_COUNT $DEFAULT_WORD_LEN "$DELIM")
+PASSPHRASE=$PASSPHRASE$(digits $DEFAULT_DIGITS_COUNT)$PAD$PAD
 
 # Add linefead if output is output is `stdout`
-[ -t 1 ] && PASSPHRASE=$PASSPHRASE'\n'
-
-printf -- $PASSPHRASE
-
+if [ -t 1 ] ; then
+	echo "$PASSPHRASE"
+else
+	echo -n "$PASSPHRASE"
+fi
