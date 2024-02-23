38a30409b957566457a90390c94171079bb9fbd9
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-02-23 07:46:31 +0200

    Refactor: Use a function, to count, and print the keys.

--- bin/countkeys.sh
+++ bin/countkeys.sh
@@ -1,47 +1,109 @@
 #!/bin/dash
+
+#---- Functions ----------------------------------------------------------------
+pad=10
+print_count()
+{
+	key=$1
+	key_ex=$2
+	fname="$3"
+	[ -z "$fname" ] && fname="/dev/stdin"
+
+	count=$(grep -Eao "$key_ex" < $fname | wc -l)
+	printf "%${pad}d %s\n" $count $key
+}
+
+#---- Parameters ---------------------------------------------------------------
 myname=$(basename $0)
 keylog=$(mktemp /tmp/$myname.XXXXX)
-stats=$(mktemp /tmp/$myname.XXXXX)
+printable=$(mktemp /tmp/$myname.XXXXX)
+modkeys=$(mktemp /tmp/$myname.XXXXX)
+commonkeys=$(mktemp /tmp/$myname.XXXXX)
+symbolkeys=$(mktemp /tmp/$myname.XXXXX)
+digitkeys=$(mktemp /tmp/$myname.XXXXX)
+letterkeys=$(mktemp /tmp/$myname.XXXXX)
 
+
+#---- Main ---------------------------------------------------------------------
+# Copy keys' log, and remove timestamps
 doas cp /var/log/logkeys.log $keylog
+# doas tail -n5000 /var/log/logkeys.log > $keylog
+sed -e '/^Logging st/d' -i $keylog
 sed -e 's/^.* > //g' -i $keylog
+sed -e 's/<[^<>]*>//g' $keylog > $printable
 
 # Mod Keys
-alt_count=$(grep -ao '<\(LAlt\|RAlt\|AltGr\)>' $keylog | wc -l)
-ctrl_count=$(grep -ao '<.Ctrl>' $keylog | wc -l)
-shift_count=$(grep -ao '<.Shft>' $keylog | wc -l)
-super_count=$(grep -ao '<.Meta>' $keylog | wc -l)
+print_count 'Shift' '<.Shft>' < $keylog > $modkeys
+print_count 'Ctrl'  '<.Ctrl>' < $keylog >> $modkeys
+print_count 'Super' '<.Meta>' < $keylog >> $modkeys
+print_count 'Alt'   '<(LAlt|RAlt|AltGr)>' < $keylog >> $modkeys
+
+print_count 'Ctrl+Shift'    '(<.Ctrl><.Shft>|<.Shft><.Ctrl>)' < $keylog >> $modkeys
+print_count 'Alt+Shift'    '(<.Alt.*><.Shft>|<.Shft><.Alt.*>)' < $keylog >> $modkeys
+print_count 'Alt+Ctrl'    '(<.Alt.*><.Ctrl>|<.Ctrl><.Alt.*>)' < $keylog >> $modkeys
+
+print_count 'Super+Shift'   '(<.Shft><.Meta>|<.Meta><.Shft>)' < $keylog >> $modkeys
+print_count 'Super+Ctrl'    '(<.Ctrl><.Meta>|<.Meta><.Ctrl>)' < $keylog >> $modkeys
+print_count 'Super+Alt'    '(<.Alt.*><.Meta>|<.Meta><.Alt.*>)' < $keylog >> $modkeys
 
 # Common Keys
-backspace_count=$(grep -ao '<BckSp>' $keylog | wc -l)
-delete_count=$(grep -ao '<Del>' $keylog | wc -l)
-enter_count=$(grep -ao '<Enter>' $keylog | wc -l)
-space_count=$(grep -ao ' ' $keylog | wc -l)
+print_count 'Space'     ' '         < $keylog > $commonkeys
+print_count 'Enter'     '<Enter>'   < $keylog >> $commonkeys
+print_count 'Backspace' '<BckSp>'   < $keylog >> $commonkeys
+print_count 'Tab'       '<Tab>'     < $keylog >> $commonkeys
+print_count 'Escape'    '<Esc>'     < $keylog >> $commonkeys
+print_count 'Delete'    '<Del>'     < $keylog >> $commonkeys
 
-end_count=$(grep -ao '<End>' $keylog | wc -l)
-esc_count=$(grep -ao '<\(Esc\|CpsLk\)>' $keylog | wc -l)
-home_count=$(grep -ao '<Home>' $keylog | wc -l)
-tab_count=$(grep -ao '<Tab>' $keylog | wc -l)
+# print_count 'End'       '<End>' < $keylog >> $commonkeys
+# print_count 'Home'      '<Home>' < $keylog >> $commonkeys
 
-pad=10
-printf "%${pad}d Super\n" $super_count > $stats
-printf "%${pad}d Alt\n" $alt_count >> $stats
-printf "%${pad}d Ctrl\n" $ctrl_count >> $stats
-printf "%${pad}d Shift\n" $shift_count >> $stats
+# Symbol Keys
+symbols="'"' ! " # $ %% & ( ) \* + , - . \/ : ; < = > ? @ [ \\ ] ^ _ ` { | } ~'
+for c in $symbols; do
+	count=$(tr -cd "$c" < $printable | wc -m)
+	printf "%${pad}d $c \n" $count >> $symbolkeys
+done
+
+# Numbers
+digits='0 1 2 3 4 5 6 7 8 9'
+for c in $digits; do
+	count=$(tr -cd "$c" < $printable | wc -m)
+	printf "%${pad}d $c \n" $count >> $digitkeys
+done
+
+# Letters
+letters='A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'
+for c in $letters; do
+	count=$(grep -Fiao "$c" $printable | wc -l)
+	printf "%${pad}d $c \n" $count >> $letterkeys
+done
+
+#---- Results ------------------------------------------------------------------
+echo "# Mod Keys"
+echo "------------"
+sort --numeric-sort --reverse < $modkeys
 
 
-printf "%${pad}d Enter\n" $enter_count >> $stats
-printf "%${pad}d Space\n" $space_count >> $stats
-printf "%${pad}d Backspace\n" $backspace_count >> $stats
-printf "%${pad}d Delete\n" $delete_count >> $stats
-printf "%${pad}d Escape\n" $esc_count >> $stats
-printf "%${pad}d Tab\n" $tab_count >> $stats
-
-# printf "%${pad}d Home\n" $home_count >> $stats
-# printf "%${pad}d End\n" $end_count >> $stats
-# printf "%${pad}d Delete\n" $delete_count >> $stats
-
-sort --numeric-sort --reverse < $stats
+printf "\n\n"
+echo "# Common Keys"
+echo "------------"
+sort --numeric-sort --reverse < $commonkeys
 
 
-rm -f $keylog $stats
+printf "\n\n"
+echo "# Symbols' Keys"
+echo "----------------"
+sort --numeric-sort --reverse < $symbolkeys
+
+printf "\n\n"
+echo "# Digits' Keys"
+echo "----------------"
+sort --numeric-sort --reverse < $digitkeys
+
+printf "\n\n"
+echo "# Letters' Keys"
+echo "----------------"
+sort --numeric-sort --reverse < $letterkeys
+
+#---- Clean-up -----------------------------------------------------------------
+rm -f $keylog $modkeys $commonkeys $printable $digitkeys $symbolkeys $letterkeys
