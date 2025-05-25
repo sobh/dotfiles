86ce0c32dea34c381aa124061319fb8b0163dca1
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2025-05-25 12:41:47 +0300

    NeoVim | Formatting : Added PHP, and XML formatters

--- .config/nvim/lua/sobh/plugins/code/formatter.lua
+++ .config/nvim/lua/sobh/plugins/code/formatter.lua
@@ -10,7 +10,27 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				php = { "php" },
 				python = { "isort", "black" },
+				xml = { "xml" },
+			},
+			formatters = {
+				php = {
+					command = "php-cs-fixer",
+					args = {
+						"fix",
+						"--config=.php-cs-fixer.php",
+						"$FILENAME",
+					},
+					stdin = false,
+					cwd = require("conform.util").root_file({ ".php-cs-fixer.php" }),
+					env = { PHP_CS_FIXER_IGNORE_ENV = 1 },
+				},
+				xml = {
+					command = "xmllint",
+					args = { "--format", "-" },
+					env = { XMLLINT_INDENT = "\t" },
+				},
 			},
 		})
 		-- Load Key Mappings
